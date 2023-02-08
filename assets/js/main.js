e2ea491ae906081276c1643a7aa3f90dfd185c1d
jekk18 <chelidzejeko9123741jeko@gmail.com>
2023-02-08 18:49:22 +0400

    home-page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,75 @@
-$(window).scroll(function() {
+$(document).ready(function(){
+    $('.slider_1').slick({  
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: $('.left-arrow'),
+        prevArrow: $('.right-arrow'),
+        responsive: [
+          {
+            breakpoint: 1638,
+            settings: {
+              centerMode: true,
+              centerPadding: '90px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 1399,
+            settings: {
+              centerMode: true,
+              centerPadding: '90px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 1199,
+            settings: {
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              centerMode: true,
+              centerPadding: '60px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 574,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '90px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+  });
+
+
+
+$(window).scroll(() => {
+    if(scrollY > 10){ 
+        $( ".header-1-box" ).addClass('scroll-header-class');
+    }else{
+        $( ".header-1-box" ).removeClass('scroll-header-class');
+    }
+});
+
+$(document).ready(() => {
     if(scrollY > 10){ 
         $( ".header-1-box" ).addClass('scroll-header-class');
     }else{
