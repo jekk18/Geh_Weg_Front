e496bf81b41cc8552f21578fcf9bb65a4b957968
jekk18 <chelidzejeko9123741jeko@gmail.com>
2023-02-22 13:14:10 +0400

    Add scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,240 @@ $(document).ready(()=> {
     $('.file-text').html(inpSplit[index]);
   });
 });
+
+
+
+if($(window).width() >= 992){
+   
+  ScrollReveal().reveal('.home-banner-nav', {
+    delay: 400,
+    duration: 600,
+    distance: '300px',
+    origin: 'right',
+    reset: false,
+  }); 
+  ScrollReveal().reveal('.home-banner-image', {
+    delay: 400,
+    duration: 600,
+    distance: '300px',
+    origin: 'left',
+    reset: false,
+  }); 
+  ScrollReveal().reveal('.home-banner-text', {
+    delay: 600,
+    duration: 600,
+    distance: '0', 
+    reset: false,
+  }); 
+  ScrollReveal().reveal('.important-title', {
+    delay: 500,
+    duration: 300,
+    distance: '0', 
+    reset: false,
+  }); 
+  ScrollReveal().reveal('.programs-box', {
+    delay: 600,
+    duration: 600,
+    distance: '200px',
+    origin: 'bottom',
+    reset: false,
+  }); 
+  ScrollReveal().reveal('.vacancy-section', {
+    delay: 600,
+    duration: 400,
+    distance: '0', 
+    reset: false,
+  }); 
+  ScrollReveal().reveal('.vacancy-box', {
+    delay: 600,
+    duration: 500,
+    distance: '300px',
+    origin: 'left', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.vacancy-type-list .vacancy-item:nth-child(1)', {
+    delay: 600,
+    duration: 700,
+    distance: '100px',
+    origin: 'left', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.vacancy-type-list .vacancy-item:nth-child(2)', {
+    delay: 600,
+    duration: 700,
+    distance: '150px',
+    origin: 'left', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.vacancy-type-list .vacancy-item:nth-child(3)', {
+    delay: 600,
+    duration: 700,
+    distance: '200px',
+    origin: 'left', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.vacancy-type-list .vacancy-item:nth-child(4)', {
+    delay: 600,
+    duration: 700,
+    distance: '250px',
+    origin: 'left', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.vacancy-right-box', {
+    delay: 600,
+    duration: 700,
+    distance: '250px',
+    origin: 'right', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.__about-left-image-box', {
+    delay: 500,
+    duration: 500,
+    distance: '250px',
+    origin: 'left', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.right-about-box', {
+    delay: 600,
+    duration: 700,
+    distance: '250px',
+    origin: 'right', 
+    reset: false,
+  });
+  ScrollReveal().reveal('.about-subscribe-box', {
+    delay: 800,
+    duration: 900,
+    distance: '0',  
+    reset: false,
+  });
+  ScrollReveal().reveal('.home-slider-section', {
+    delay: 400,
+    duration: 400,
+    distance: '0',  
+    reset: false,
+  });
+  ScrollReveal().reveal('.home-slider', {
+    delay: 600,
+    duration: 600,
+    distance: '0',  
+    origin: 'bottom',
+    reset: false,
+  });
+  ScrollReveal().reveal('.footer-logo-box', {
+    delay: 400,
+    duration: 400,
+    distance: '150px',  
+    origin: 'left',
+    reset: false,
+  });
+  ScrollReveal().reveal('.footer-contact-info', {
+    delay: 400,
+    duration: 400,
+    distance: '0',   
+    reset: false,
+  });
+  ScrollReveal().reveal('.footer-links', {
+    delay: 400,
+    duration: 400,
+    distance: '0',   
+    reset: false,
+  });
+  ScrollReveal().reveal('.footer-message-box', {
+    delay: 400,
+    duration: 400,
+    distance: '150px',  
+    origin: 'right',   
+    reset: false,
+  });
+  ScrollReveal().reveal('._banner', {
+    delay: 400,
+    duration: 800,
+    distance: '0',    
+    reset: false,
+  });
+  ScrollReveal().reveal('.about-content-text', {
+    delay: 400,
+    duration: 800,
+    distance: '0',   
+    reset: false,
+  });
+  ScrollReveal().reveal('.brc-cont', {
+    delay: 600,
+    duration: 800,
+    distance: '0',   
+    reset: false,
+  });
+  ScrollReveal().reveal('.contact-box', {
+    delay: 400,
+    duration: 400,
+    distance: '0',       
+    reset: false,
+  });
+  ScrollReveal().reveal('.left-contact', {
+    delay: 500,
+    duration: 500,
+    distance: '150px',  
+    origin: 'left',    
+    reset: false,
+  });
+   
+  ScrollReveal().reveal('.right-cont-form-box', {
+    delay: 700,
+    duration: 700,
+    distance: '0',       
+    reset: false,
+  });
+  ScrollReveal().reveal('.map-box', {
+    delay: 700,
+    duration: 700,
+    distance: '0',       
+    reset: false,
+  });
+  ScrollReveal().reveal('.vacancy-det-box h1', {
+    delay: 600,
+    duration: 600,
+    distance: '100px',  
+    origin: 'left',        
+    reset: false,
+  });
+  ScrollReveal().reveal('.buttons-box', {
+    delay: 600,
+    duration: 600,
+    distance: '150px',  
+    origin: 'left',        
+    reset: false,
+  });
+  ScrollReveal().reveal('.requests-box', {
+    delay: 600,
+    duration: 600,
+    distance: '200px',  
+    origin: 'left',        
+    reset: false,
+  });
+  ScrollReveal().reveal('.text-det-vac', {
+    delay: 800,
+    duration: 700,
+    distance: '0',       
+    reset: false,
+  });
+  ScrollReveal().reveal('.file-download', {
+    delay: 800,
+    duration: 700,
+    distance: '200px',  
+    origin: 'left',        
+    reset: false,
+  });
+  ScrollReveal().reveal('.socials', {
+    delay: 800,
+    duration: 700,
+    distance: '200px',  
+    origin: 'left',        
+    reset: false,
+  });
+  ScrollReveal().reveal('.v-det-right', {
+    delay: 800,
+    duration: 900,
+    distance: '0',     
+    reset: false,
+  });
+}
